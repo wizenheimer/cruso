c0d65b57c3ff4bc95493505bfb1fd09ba08fb1de
wizenheimer <xNayanKumar@gmail.com>
2025-07-13 05:20:15 +0530

    feat: add sonner core

--- lib/toast.ts
+++ lib/toast.ts
@@ -1,38 +1,38 @@
-import { toast } from "sonner"
+import { toast } from 'sonner';
 
 export const showToast = {
-  success: (message: string) => {
-    toast.success(message)
-  },
-  
-  error: (message: string) => {
-    toast.error(message)
-  },
-  
-  info: (message: string) => {
-    toast.info(message)
-  },
-  
-  warning: (message: string) => {
-    toast.warning(message)
-  },
-  
-  loading: (message: string) => {
-    return toast.loading(message)
-  },
-  
-  promise: <T>(
-    promise: Promise<T>,
-    options: {
-      loading: string
-      success: string | ((data: T) => string)
-      error: string | ((error: Error) => string)
-    }
-  ) => {
-    return toast.promise(promise, options)
-  },
-  
-  dismiss: (toastId?: string | number) => {
-    toast.dismiss(toastId)
-  }
-}
+    success: (message: string) => {
+        toast.success(message);
+    },
+
+    error: (message: string) => {
+        toast.error(message);
+    },
+
+    info: (message: string) => {
+        toast.info(message);
+    },
+
+    warning: (message: string) => {
+        toast.warning(message);
+    },
+
+    loading: (message: string) => {
+        return toast.loading(message);
+    },
+
+    promise: <T>(
+        promise: Promise<T>,
+        options: {
+            loading: string;
+            success: string | ((data: T) => string);
+            error: string | ((error: Error) => string);
+        },
+    ) => {
+        return toast.promise(promise, options);
+    },
+
+    dismiss: (toastId?: string | number) => {
+        toast.dismiss(toastId);
+    },
+};
