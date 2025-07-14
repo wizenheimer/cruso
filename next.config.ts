68f001a7b85919f74b966ed79bf590aa92942fe8
wizenheimer <xNayanKumar@gmail.com>
2025-07-14 23:41:39 +0530

    build: initialize mastra core

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
+    serverExternalPackages: ['@mastra/*'],
     output: 'standalone',
 };
 
